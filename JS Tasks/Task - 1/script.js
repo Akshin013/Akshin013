f30868e70d96521e5e4a277b32e066a6c872905b
Akshin013 <akshingadimov@gmail.com>
2025-01-26 21:52:08 +0400

    JS Tasks

---
+++ JS Tasks/Task - 1/script.js
@@ -0,0 +1,138 @@
+// Task 1  /////////////////////////////////////
+
+//console.log("My name is Ugur")
+
+
+// Task 2 //////////////////////////////
+
+//let name = "Akshin"
+//console.log(name)
+
+
+//// Task 3 //////////////////////////////////////////////////////////
+
+//- 2user        olmaz
+//- c@rd         olmaz
+//- person       olar
+//- title color  olmaz
+//- console      olar 
+//- $add         olar 
+//
+
+
+//// Task 4////////////////////////////////////////////////////////
+//
+//let bookPrice = 750;
+//let bookAmount = 14;
+//
+//let a = 750; // bookPrice
+//let b = 14; // bookAmount
+//
+//let result =a*b; //result
+//console.log(result);
+//
+
+
+
+
+////Task 5 //////////////////////////
+
+// 1km/h = 0.277778 m/s
+
+//let kmhSpeed = prompt("Km/h yazin"); //km h
+//let msSpeed = (kmhSpeed * 0.277778); //m s
+//console.log(msSpeed);
+
+
+// Task 6 /////////////////////////////////////////////////////
+
+//var text = "year";
+//let year = prompt(text);
+//
+//var text2 = "month"
+//let month = prompt(text2);
+//
+//var text3 = "day"
+//let day = prompt(text3);
+//
+//
+//
+//
+//console.log(year + '.' + month + '.' + day );
+//
+//let year1 = 2025 - year;
+//console.log(year1);
+//
+//let month1 = 13 - month;
+//console.log(month1);
+//
+//let day1 = 30 - day;
+//console.log(day1);
+//
+//
+//
+//let year2 =  year1 * 365;
+//console.log(year2)
+//
+//let month2 = month1 * 12;
+//console.log(month2);
+//
+//let day2 = day1 * 30;
+//console.log(day2);
+//
+//let res = year2 + month2 + day2;
+//
+//console.log(res)
+//
+//console.log(res/365) 
+
+
+// Task 7 ////////////////////////////////////////////////////
+
+
+//let firstName = "Alan";
+//let lastName = "Turing"
+//
+//console.log('Sizin adınız:' + ' ' + firstName + ' ' + lastName + '.' + ' '  +'Tanışlığımıza çox şadam!')
+
+
+
+// Task 8 ///////////////////////////////////////////////////
+
+//let ad = prompt("Adinizi yazin");
+//console.log('Salam,' + ' ' + ad + ' ' + '!');
+
+
+// Task 9 
+
+//let num1 = prompt("Num 1");
+//let num2  = prompt("Num 2");
+//
+//
+//Number(num1);
+//Number(num2);
+//
+//console.log(typeof "num1")
+//let operation1 = (num1 + num2);
+//console.log(operation1);
+//
+//let operation2 = (num1 - num2);
+//console.log(operation2);
+//
+//let operation3 = (num1 * num2);
+//console.log(operation3);
+//
+//let operation4 = (num1 / num2);
+//console.log(operation4);
+
+//Task 10 ////////////////////////////////////////////////////////
+
+//let megleb = Number.parseInt(prompt("megleb"));
+//let faiz   = Number.parseInt(prompt("faiz"));
+//let muddet = Number.parseInt(prompt("muddet"));
+//
+//
+//let result = (megleb * faiz * muddet);
+//let theEndResult =(result / "100");
+//console.log(result);
+//console.log(theEndResult);
---
+++ JS Tasks/Task - 2/script.js
@@ -0,0 +1,44 @@
+// Task 1 ////////
+
+//let a = prompt("1-ci eded")
+//let b = prompt("2-ci eded")
+//
+//
+//let message = a > b ? "a"
+//: "b"
+//console.log(message);
+
+
+// Task 2 ////////
+
+//let a = prompt("1-ci eded")
+//let b = prompt("2-ci eded")
+//
+//let message = a == b ? "cut eded" 
+//: "cut deil"
+//console.log(message);
+
+
+// Task 3 ////////
+
+/// admin boyuk herifden yazilanda kececek
+
+//let a = prompt("Siz kimsiz?")
+//let b = "admin"
+//
+//let message = a === b? "Salam,admin. Səhifyə daxil oldun"
+//: "Səni tanımıram. Daxil ola bilməzsən"
+//console.log(message);
+
+
+// Task 4 
+
+//let a = Number.parseInt(prompt("a"));
+//let b = Number.parseInt(prompt("b"));
+//
+//let c = a + b;
+//console.log("Eded" + ' ' + c);
+//
+//let message = a >= 100? "ədədlərin cəmi 100-ü keçdi" 
+//: "ədədlərin cəmi 100-dn kiçikdir";
+//console.log(message);
