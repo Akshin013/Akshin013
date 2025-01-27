978bde9c4430e4398e73e67275c2898f77b07742
Akshin013 <akshingadimov@gmail.com>
2025-01-28 00:07:53 +0400

    Task 3 JS

---
+++ JS Tasks/Task - 3/script.js
@@ -0,0 +1,186 @@
+// Task 1 //////////////////////
+//let a = +prompt("Ocenka");
+//console.log(a);
+//
+//if(a >= 0 & a<= 100)    
+//    if(a >= 90){
+//        console.log("A")
+//    } if(a >= 80 & a <= 89){
+//        console.log("B");
+//    } if(a >= 70 & a <= 79){
+//        console.log("C");
+//    } if(a >= 60 & a <= 69){
+//        console.log("D");
+//    } if(a <= 60 ){
+//        console.log("F");
+//    } if(a > 85){
+//        console.log("Ela")
+//    } if(a >= 50 & a < 85){
+//        console.log("Yaxsi")
+//    } if(a >50){
+//        console.log("Kesr")
+//    }
+//else{
+//    console.log("Error")
+//}
+//
+//
+//
+//
+//
+
+// Task 2 /////////////////////
+
+//let a = +prompt("Havani yazin")
+//console.log(a);
+//if(a >= 25){
+//    console.log("isti")
+//} if(a >= 10 & a < 25){
+//    console.log("normal")
+//} if(a >= 0 & a < 10){
+//    console.log("soyuq")
+//} if(a < 0){
+//    console.log("şaxta")
+//} else{
+//    console.log("Tezeden yazin")
+//}
+//
+
+// Task 3 /////////////////////
+
+//let username = "Akshin";
+//let password = "Akshin2006";
+//
+//let a = prompt("username");
+//let b = prompt("password");
+//
+//if(a === username & b === password){
+//    console.log("Duzdu siz kecdiz !")
+//} else {
+//    console.log("Error")
+//}
+//
+
+// Task 4 //////////////////////
+
+//let yash = +prompt("Salam, yashinizi yazin !")
+//
+//let a = "bilet qiyməti 5  AZN";
+//let b = "bilet qiyməti 10 AZN";
+//let c = "bilet qiyməti 15 AZN";
+//
+//if(yash < 12 & yash > 0){
+//    console.log(a)
+//} if(yash >= 12 & yash < 18){
+//    console.log(b);
+//} if(yash > 18){
+//    console.log(c);
+//} else{
+//    console.log("")
+//}
+
+
+
+// Task 5 /////////////////////
+
+//let a = prompt("Ish gunu yazin");
+//
+//// Ish gunu
+//let b = "Bazar ertəsi";
+//let c = "Çərşənbə axşamı";
+//let d = "Çərşənbə";
+//let e = "Cümə axşamı";
+//let f = "Cümə";
+//
+//
+//// Istirahet gunu 
+//let g = "Şənbə";
+//let h = "Bazar"; 
+//
+//if(a === b || a === b || a === c || a === d || a === e || a === f){
+//    console.log("İş günidi");
+//} if(a === g || a === h){
+//    console.log("İstirahət günidi");
+//} else{
+//    console.log("Error")
+//}
+
+
+// Task 6 ////////////
+
+//let a = +prompt("Sürətini yazin")
+//
+//// 60-dan aşağ 
+//let b = "Sürət çox yavaşdır.";
+//
+////60-120
+//let c =  "Normal sürətlə hərəkət edirsiniz.";
+//
+////  120-dən yuxarı
+//let d = "Sürət çox yüksəkdir, təhlükəlidir!";
+//
+//if(a <= 60) {
+//    console.log(b);
+//} if(a > 60 & a < 120){
+//    console.log(c)
+//} if(a >= 120) {
+//    console.log(d)
+//} else {
+//    console.log("Error");
+//}
+
+
+
+// Task 7 //////////////////////
+
+//let a = +prompt("Saati yazin");
+//
+////  6-dan 12-dək
+//let b = "Səhərdir.";
+//
+////  12-dən 18-dək
+//let c = "Gündüzdür.";
+//
+////  18-dən 24-dək
+//let d = "Axşamdır";
+//
+////  0-dan 6-dək
+//let e = "Gecədir.";
+//
+//if(a >= 0 & a <= 24){
+//    if(a > 6 & a < 12){
+//        console.log(b)
+//    } if(a >= 12 & a < 18){
+//        console.log(c)
+//    } if(a >= 18 & a <= 24){
+//        console.log(d)
+//    } if(a >= 0 & a < 6){
+//        console.log(e)
+//    }
+//} else {
+//    console.log("Error")
+//}
+
+
+// Task 8 /////////////////////
+
+//let a = prompt("Rengi yazin");
+//
+//let b = "Qırmızı";
+//let c = "Sarı";
+//let d = "Yaşıl";
+//
+//
+//
+//if( a === b & a === c & a === d){
+//    if( a === b){
+//        console.log("Dayanın.");
+//    } if( a === c){
+//        console.log("Hazırlaşın.");
+//    } if( a === d){
+//        console.log("Hərəkət edin.");
+//    }
+//}
+//    else{
+//    console.log("Yanlış rəng daxil etdiniz.");
+//}
